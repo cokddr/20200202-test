c6ad04c82749a7ac0af737006bd21d48a30a492c
appear <appear.ko@gmail.com>
2020-02-09 12:48:35 +0900

    global style 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import Navbar from './navbar'
+import GlobalStyles from './global-styles'
 
 function App() {
   const [title, setTitle] = useState('App 페이지의 Navbar')
 
   return (
     <div>
+      <GlobalStyles />
       <Navbar title={title} />
     </div>
   )
